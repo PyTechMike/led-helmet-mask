03d5ada063222114bb9b129726d055567317e01f
PyTechMike <pytechmike@gmail.com>
2019-12-25 23:56:43 +0200

    Lastest code version uploaded

--- bt-test/bt-test.ino
+++ bt-test/bt-test.ino
@@ -6,8 +6,8 @@ FASTLED_USING_NAMESPACE
 #endif
 
 #define DATA_PIN    12
-#define LED_TYPE    WS2812
-#define COLOR_ORDER GRB
+#define LED_TYPE    WS2811
+#define COLOR_ORDER RGB
 #define NUM_LEDS    42
 CRGB leds[NUM_LEDS];
 
